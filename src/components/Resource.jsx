4f60476849560c3cc5e972854675520203eae734
codenameMANUEL <manuelfranky0@gmail.com>
2024-02-08 15:06:10 +0100

    resource component done

--- src/components/Resource.jsx
+++ src/components/Resource.jsx
@@ -1,31 +1,38 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { ThreeDotsVertical, ArrowRightShort } from 'react-bootstrap-icons';
-import facebook from "../assets/image/Logo.png"
-import medium from "../assets/image/Medium.png"
-import google from "../assets/image/Google.png"
-import youtube from "../assets/image/Youtube.png"
-import bing from "../assets/image/Bing.png"
+//import facebook from "../assets/image/Logo.png"
+//import medium from "../assets/image/Medium.png"
+//import google from "../assets/image/Google.png"
+//import youtube from "../assets/image/Youtube.png"
+//import bing from "../assets/image/Bing.png"
 import { BASE_URL, USER_DOMAIN } from '../utils/config';
 
 
 function Resource() {
 
-    const [resources, setresources] = useState([])
+    const [resources, setResources] = useState([])
     const [error, setError] = useState(null)
 
     useEffect(()=>{
         const fetchResources = async () =>{
             try {
                 const res = await axios.get(`${BASE_URL}${USER_DOMAIN}/resources`);
-                setresources(res.resources.data)
-                console.log("hey")
+                console.log( 'entered:',res.data.data)
+                setResources(res.data.data)
+                // console.log("hey")
             } catch (error){
                 setError(error);
             }
         };
         fetchResources();
     }, []);
+
+    
+    const formatTimestampToDate = (timestamp) => {
+        const date = new Date(timestamp);
+        return date.toLocaleString();
+    };
 
     return (
         <div className="container card pb-4">
@@ -38,20 +45,20 @@ function Resource() {
                   <th scope="col">MOOD TYPE</th>
                   <th scope="col">DURATION</th>
                   <th scope="col">TIME CREATED</th>
-                  <th scope="col">&nbsp;</th>
+                  <th scope="col"></th>
                 </tr>
               </thead>
               <tbody>
-
+                 
                 {resources.map(resource=>{
-                    <tr key={resource.id}>
-                    <th scope="row"><span><img src={resource.image} alt="" className="socialImage" /></span> Facebook</th>
-                    <td>12,5564</td>
+                    return <tr key={resource._id}>
+                    <th scope="row"><span><img src={resource.image} alt="" className="socialImage" /></span> </th>
+                    <td>{resource.title}</td>
                     <td>
-                      <button className="change-btn-green">+65.31%</button>
+                      <button className="change-btn-green">{resource.moodType}</button>
                     </td>
-                    <td>00:08:10</td>
-                    <td>21.32%</td>
+                    <td>{resource.duration}</td>
+                    <td>{formatTimestampToDate(resource.time_created)}</td>
                     <td>
                       <li className="nav-item dropdown no-arrow">
                         <a className="nav-link dropdown-toggle" href="#" id="userDropdown"
@@ -76,131 +83,6 @@ function Resource() {
                     </td>
                   </tr>
                 })}
-
-                <tr>
-                  <th scope="row"><span><img src={medium} alt="" className="socialImage" /></span>Medium</th>
-                  <td>9,567</td>
-                  <td>
-                    <button className="change-btn-green">+65.31%</button>
-                  </td>
-                  <td>00:08:10</td>
-                  <td>21.32%</td>
-                  <td>
-                    <li className="nav-item dropdown no-arrow">
-                      <a className="nav-link dropdown-toggle" href="#" id="userDropdown"
-                        role="button" data-toggle="dropdown" aria-haspopup="true"
-                        aria-expanded="false">
-                        <i className="bi bi-three-dots-vertical"></i>
-                      </a>
-                      <div className="dropdown-menu dropdown-menu-left animated--grow-in"
-                        aria-labelledby="userDropdown">
-                        <a className="dropdown-item" href="#">
-                          Approve
-                        </a>
-                        <a className="dropdown-item" href="#">
-                          Send feedback
-                        </a>
-                        <a className="dropdown-item text-danger" href="#" data-toggle="modal"
-                          data-target="">
-                          Decline
-                        </a>
-                      </div>
-                    </li>
-                  </td>
-                </tr>
-                <tr>
-                  <th scope="row"><span><img src={google} alt="" className="socialImage" /></span>Google</th>
-                  <td>5,440</td>
-                  <td>
-                    <button className="change-btn-red">+65.31%</button>
-                  </td>
-                  <td>00:08:10</td>
-                  <td>21.32%</td>
-                  <td>
-                    <li className="nav-item dropdown no-arrow">
-                      <a className="nav-link dropdown-toggle" href="#" id="userDropdown"
-                        role="button" data-toggle="dropdown" aria-haspopup="true"
-                        aria-expanded="false">
-                        <i className="bi bi-three-dots-vertical"></i>
-                      </a>
-                      <div className="dropdown-menu dropdown-menu-left animated--grow-in"
-                        aria-labelledby="userDropdown">
-                        <a className="dropdown-item" href="#">
-                          Approve
-                        </a>
-                        <a className="dropdown-item" href="#">
-                          Send feedback
-                        </a>
-                        <a className="dropdown-item text-danger" href="#" data-toggle="modal"
-                          data-target="">
-                          Decline
-                        </a>
-                      </div>
-                    </li>
-                  </td>
-                </tr>
-                <tr>
-                  <th scope="row"><span><img src={youtube} alt="" className="socialImage" /></span>Youtube</th>
-                  <td>2,767</td>
-                  <td>
-                    <button className="change-btn-green">+65.31%</button>
-                  </td>
-                  <td>00:08:10</td>
-                  <td>21.32%</td>
-                  <td>
-                    <li className="nav-item dropdown no-arrow">
-                      <a className="nav-link dropdown-toggle" href="#" id="userDropdown"
-                        role="button" data-toggle="dropdown" aria-haspopup="true"
-                        aria-expanded="false">
-                        <i className="bi bi-three-dots-vertical"></i>
-                      </a>
-                      <div className="dropdown-menu dropdown-menu-left animated--grow-in"
-                        aria-labelledby="userDropdown">
-                        <a className="dropdown-item" href="#">
-                          Approve
-                        </a>
-                        <a className="dropdown-item" href="#">
-                          Send feedback
-                        </a>
-                        <a className="dropdown-item text-danger" href="#" data-toggle="modal"
-                          data-target="">
-                          Decline
-                        </a>
-                      </div>
-                    </li>
-                  </td>
-                </tr>
-                <tr>
-                  <th scope="row"><span><img src={bing} alt="" className="socialImage" /></span>Bing</th>
-                  <td>1,443</td>
-                  <td>
-                    <button className="change-btn-red">+65.31%</button>
-                  </td>
-                  <td>00:08:10</td>
-                  <td>21.32%</td>
-                  <td>
-                    <li className="nav-item dropdown no-arrow">
-                      <a className="nav-link dropdown-toggle" href="#" id="userDropdown"
-                        role="button" data-toggle="dropdown" aria-haspopup="true"
-                        aria-expanded="false">
-                        <i className="bi bi-three-dots-vertical"></i>
-                      </a>
-                      <div className="dropdown-menu dropdown-menu-left animated--grow-in"
-                        aria-labelledby="userDropdown">
-                        <a className="dropdown-item" href="#">
-                          Approve
-                        </a>
-                        <a className="dropdown-item" href="#">
-                          Send feedback
-                        </a>
-                        <a className="dropdown-item text-danger" href="#" data-toggle="modal"
-                          data-target="">
-                          Decline
-                        </a>
-                      </div>
-                    </li>
-                  </td>
-                </tr>
               </tbody>
             </table>
           </div>
